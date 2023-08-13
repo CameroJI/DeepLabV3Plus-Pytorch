0f329d006ee40d945127f055fe964e30a17937d6
CameroJI <137007386+CameroJI@users.noreply.github.com>
2023-08-13 14:04:10 -0600

    Update cityscapes.py

--- datasets/cityscapes.py
+++ datasets/cityscapes.py
@@ -58,8 +58,6 @@ class Cityscapes(data.Dataset):
         self.targets_dir = os.path.join(f'{root}/gtFine/labels', split)
         self.transform = transform
 
-        print(f'{root}/gtFine/images')
-
         self.split = split
         self.images = []
         self.targets = []
